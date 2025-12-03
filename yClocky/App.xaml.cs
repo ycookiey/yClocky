f3109926ae31d419502b0fac08ab13c794c17f34
ycookiey <70356861+ycookiey@users.noreply.github.com>
2025-12-03 13:30:12 +0900

    Initialize WPF project

---
+++ yClocky/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace yClocky;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
