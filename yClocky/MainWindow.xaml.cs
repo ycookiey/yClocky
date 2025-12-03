b46d586861a598d0fbf9540d0442e8b71309cb98
ycookiey <70356861+ycookiey@users.noreply.github.com>
2025-12-03 13:35:04 +0900

    Fix MainWindow logic

--- yClocky/MainWindow.xaml.cs
+++ yClocky/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace yClocky;
 
@@ -16,8 +17,40 @@ namespace yClocky;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private DispatcherTimer _timer;
+
     public MainWindow()
     {
         InitializeComponent();
+        InitializeClock();
+    }
+
+    private void InitializeClock()
+    {
+        _timer = new DispatcherTimer();
+        _timer.Interval = TimeSpan.FromSeconds(0.1); // Update frequently for smooth seconds
+        _timer.Tick += Timer_Tick;
+        _timer.Start();
+        UpdateClock(); // Initial update
+    }
+
+    private void Timer_Tick(object? sender, EventArgs e)
+    {
+        UpdateClock();
+    }
+
+    private void UpdateClock()
+    {
+        var now = DateTime.Now;
+        TimeText.Text = now.ToString("HH:mm:ss");
+        DateText.Text = now.ToString("yyyy/MM/dd (ddd)");
+    }
+
+    private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ButtonState == MouseButtonState.Pressed)
+        {
+            this.DragMove();
+        }
     }
 }
