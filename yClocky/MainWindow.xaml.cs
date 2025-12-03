d77ba08367e66e7e020a22c927848331fdbd7ac9
ycookiey <70356861+ycookiey@users.noreply.github.com>
2025-12-03 13:47:37 +0900

    Implement Stealth Mode and Interactions

--- yClocky/MainWindow.xaml.cs
+++ yClocky/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
-﻿using System.Text;
+﻿using System.Runtime.InteropServices;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -28,10 +30,10 @@ public partial class MainWindow : Window
     private void InitializeClock()
     {
         _timer = new DispatcherTimer();
-        _timer.Interval = TimeSpan.FromSeconds(0.1); // Update frequently for smooth seconds
+        _timer.Interval = TimeSpan.FromSeconds(0.1);
         _timer.Tick += Timer_Tick;
         _timer.Start();
-        UpdateClock(); // Initial update
+        UpdateClock();
     }
 
     private void Timer_Tick(object? sender, EventArgs e)
@@ -50,7 +52,54 @@ public partial class MainWindow : Window
     {
         if (e.ButtonState == MouseButtonState.Pressed)
         {
-            this.DragMove();
+            if (Keyboard.Modifiers == ModifierKeys.Control)
+            {
+                var startLeft = this.Left;
+                var startTop = this.Top;
+
+                try
+                {
+                    this.DragMove();
+                }
+                catch { } // Ignore errors during drag
+
+                var endLeft = this.Left;
+                var endTop = this.Top;
+
+                // If moved less than 2 pixels, treat as click
+                if (Math.Abs(startLeft - endLeft) < 2 && Math.Abs(startTop - endTop) < 2)
+                {
+                    OpenSettings();
+                }
+            }
         }
     }
+
+    private void OpenSettings()
+    {
+        // Placeholder for Settings Window
+        MessageBox.Show("Settings Window will open here.", "yClocky Settings");
+    }
+
+    protected override void OnSourceInitialized(EventArgs e)
+    {
+        base.OnSourceInitialized(e);
+        SetWindowStyle();
+    }
+
+    private void SetWindowStyle()
+    {
+        var helper = new WindowInteropHelper(this);
+        int exStyle = (int)GetWindowLong(helper.Handle, GWL_EXSTYLE);
+        SetWindowLong(helper.Handle, GWL_EXSTYLE, exStyle | WS_EX_TOOLWINDOW);
+    }
+
+    private const int GWL_EXSTYLE = -20;
+    private const int WS_EX_TOOLWINDOW = 0x00000080;
+
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetWindowLong(IntPtr hWnd, int nIndex);
+
+    [DllImport("user32.dll")]
+    private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
 }
